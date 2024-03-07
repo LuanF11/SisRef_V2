d14796a5485395e18e82236e67efa1494d6f684e
LuanF11 <luaninfo09@gmail.com>
2024-03-07 16:55:11 -0300

    Adicionado a opção de bordas no botão

--- src/components/CardButton/CardButton.tsx
+++ src/components/CardButton/CardButton.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./CardButton.module.css";
 
-const CardButton = ({ backgroundColorButton, colorTextButton, contentButton }) => {
+const CardButton = ({ backgroundColorButton, colorTextButton, contentButton, borderButton = "none" }) => {
   return (
-    <button className={styles.btn_confirm} style={{backgroundColor:backgroundColorButton, color:colorTextButton}}>{contentButton}</button>
+    <button className={styles.btn_confirm} style={{backgroundColor:backgroundColorButton, color:colorTextButton, border:borderButton}}>{contentButton}</button>
   );
 };
 
