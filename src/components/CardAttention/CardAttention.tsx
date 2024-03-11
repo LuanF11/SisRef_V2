c7b4cd6724764247d9816b3e5af9284cfab3f79e
LuanF11 <luaninfo09@gmail.com>
2024-03-11 17:02:41 -0300

    Implementado a função do botao do card de atencao para sumir ao ser clicado

--- src/components/CardAttention/CardAttention.tsx
+++ src/components/CardAttention/CardAttention.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./CardAttention.module.css";
 import AlertIcon from "../Icons/AlertIcon";
 import Button from "../Button/Button";
 
-const CardAttention = ({ children }: { children: React.ReactNode }) => (
+
+
+const CardAttention = ({ children }: { children: React.ReactNode }) => {
+  const [isVisible, setIsVisible] = useState(true);
+  return(
+    isVisible && (
   <div className={styles.card}>
     <div className={styles.top}>
       <AlertIcon />
@@ -12,8 +17,10 @@ const CardAttention = ({ children }: { children: React.ReactNode }) => (
     <div className={styles.content}>
       {children}
     </div>
-    <Button variant="branco">Entendido</Button>
+    <Button variant="branco" onClick={() => setIsVisible(false)}>Entendido</Button>
   </div>
-)
+    )
+  );
+}
 
 export default CardAttention;
